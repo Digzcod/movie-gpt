78d938f0b73d5b1351355270e01bb05ecf666939
Digzcod <stephenjaramillo5@gmail.com>
2024-07-09 10:34:59 +0800

    gpt 3.5

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -6,9 +6,9 @@ export const GPT_AI = new OpenAI({
   dangerouslyAllowBrowser: true, // This is the default and can be omitted
 });
 
-// export const AI_MESSAGE = {
-//   messages: [{ role: "user", content: "Say this is a test" }],
-//   model: "gpt-3.5-turbo",
-// };
+export const AI_MESSAGE = {
+  messages: [{ role: "user", content: "Say this is a test" }],
+  model: "gpt-3.5-turbo",
+};
 export const movieSystemQuery ="Act like a movie recommendation system And suggest some movies for the query"
 export const exampleSuggestionQueryResult = ". Only give me names of 15 movies, comma separed like the example result given ahead. Example: Ghosted, Vagabound 2, Dune, Garfield 2, Stone, Red Notice"  
